8eb3652254368512617fe6ea610654ca49536727
peddadavivek1234 <peddadavicek@gmail.com>
2023-08-08 13:52:20 +0530

    Add filters

--- src/components/moviesearch/MovieSearch.js
+++ src/components/moviesearch/MovieSearch.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './movieSearch.css'
+import './movieSearch.css';
 
 const MovieSearch = () => {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortBy, setSortBy] = useState("rating"); 
+  const moviesPerPage = 5; 
 
   const API_KEY = "e8ccc676e299173067a80520c1fee405";
   const BASE_URL = "https://api.themoviedb.org/3/search/movie";
@@ -18,8 +21,24 @@ const MovieSearch = () => {
         },
       });
 
-      const sortedMovies = response.data.results.sort((a, b) => b.vote_average - a.vote_average);
+      let sortedMovies = [...response.data.results];
+
+      if (sortBy === "release_date") {
+        sortedMovies = sortedMovies.sort((a, b) => {
+          if (a.release_date && b.release_date) {
+            return new Date(a.release_date) - new Date(b.release_date);
+          } else if (!a.release_date) {
+            return 1;
+          } else {
+            return -1;
+          }
+        });
+      } else {
+        sortedMovies = sortedMovies.sort((a, b) => b.vote_average - a.vote_average);
+      }
+
       setMovies(sortedMovies);
+      setCurrentPage(1); 
     } catch (error) {
       console.error("Error fetching data: ", error);
       setMovies([]);
@@ -36,6 +55,19 @@ const MovieSearch = () => {
     }
   };
 
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleSortChange = (newSortBy) => {
+    setSortBy(newSortBy);
+    searchMovies();
+  };
+
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie);
+
   return (
     <div className="moviesearch">
       <div className="searchbar">
@@ -49,12 +81,30 @@ const MovieSearch = () => {
           placeholder="Enter movie name"
         />
         <button className="Search" onClick={handleSearch}>Search!</button>
+        <div className="sort-options">
+            <div className="sort-rating">
+                <button
+                    className={sortBy === "rating" ? "active" : ""}
+                    onClick={() => handleSortChange("rating")}
+                >
+                    Sort by Rating
+                </button>
+            </div>
+            <button
+            className={sortBy === "release_date" ? "active" : ""}
+            onClick={() => handleSortChange("release_date")}
+            >
+            Sort by Release Date
+        </button>
+      </div>
       </div>
 
-      {movies.length > 0 ? (
+      
+
+      {currentMovies.length > 0 ? (
         <div className="movie-list">
           <div className="movie-cards">
-            {movies.map((movie) => (
+            {currentMovies.map((movie) => (
               <div key={movie.id} className="movie-card">
                 <div className="movie-image">
                   <img
@@ -71,6 +121,17 @@ const MovieSearch = () => {
                   <p>{movie.overview}</p>
                 </div>
               </div>
+            ))}
+          </div>
+          <div className="pagination">
+            {Array.from({ length: Math.ceil(movies.length / moviesPerPage) }, (_, index) => (
+              <button
+                key={index}
+                onClick={() => paginate(index + 1)}
+                className={`pages ${currentPage === index + 1 ? "active" : ""}`}
+              >
+                {index + 1}
+              </button>
             ))}
           </div>
         </div>
